e25a65530ddaa647a01b4902b28920516b799e18
evgeny <wxnnvdie@gmail.com>
2025-01-05 13:16:43 +0500

    Initial Commit

---
+++ SP6/SP6App.swift
@@ -0,0 +1,17 @@
+//
+//  SP6App.swift
+//  SP6
+//
+//  Created by Евгений Михайлов on 05.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct SP6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SP6Tests/SP6Tests.swift
@@ -0,0 +1,17 @@
+//
+//  SP6Tests.swift
+//  SP6Tests
+//
+//  Created by Евгений Михайлов on 05.01.2025.
+//
+
+import Testing
+@testable import SP6
+
+struct SP6Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
